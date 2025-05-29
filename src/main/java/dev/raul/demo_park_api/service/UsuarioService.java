bf02950eb6d1990d0e65f6275aadcdbb982010ef
raulmaciel <raulmacielxd@gmail.com>
2025-05-29 09:44:30 -0300

    feat: implementando recurso para criacao de novo usuario

--- src/main/java/dev/raul/demo_park_api/service/UsuarioService.java
+++ src/main/java/dev/raul/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package dev.raul.demo_park_api.service;
 
+import dev.raul.demo_park_api.entity.Usuario;
 import dev.raul.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,4 +12,9 @@ public class UsuarioService {
 
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/dev/raul/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/dev/raul/demo_park_api/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package dev.raul.demo_park_api.web.controller;
 
+import dev.raul.demo_park_api.entity.Usuario;
 import dev.raul.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
