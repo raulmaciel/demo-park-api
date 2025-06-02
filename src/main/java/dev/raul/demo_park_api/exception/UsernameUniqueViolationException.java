4315da95cc42461668373c5c8ea6fb33fb8fc6df
raulmaciel <raulmacielxd@gmail.com>
2025-06-02 13:20:00 -0300

    feat: trata exceção para usuário não encontrado

--- src/main/java/dev/raul/demo_park_api/exception/UsernameUniqueViolationException.java
+++ src/main/java/dev/raul/demo_park_api/exception/UsernameUniqueViolationException.java
@@ -1,4 +1,7 @@
 package dev.raul.demo_park_api.exception;
 
-public class UsernameUniqueViolationException {
+public class UsernameUniqueViolationException extends RuntimeException {
+    public UsernameUniqueViolationException(String message) {
+        super(message);
+    }
 }
--- src/main/java/dev/raul/demo_park_api/service/UsuarioService.java
+++ src/main/java/dev/raul/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package dev.raul.demo_park_api.service;
 
 import dev.raul.demo_park_api.entity.Usuario;
+import dev.raul.demo_park_api.exception.EntityNotFoundException;
+import dev.raul.demo_park_api.exception.UsernameUniqueViolationException;
 import dev.raul.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,14 +18,18 @@ public class UsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        }catch (DataIntegrityViolationException e){
+            throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado!", usuario.getUsername()));
+        }
     }
 
 
     @Transactional(readOnly = true)
     public Usuario buscarPorId(Long id) {
         return usuarioRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Usuário não encontrado!")
+                () -> new EntityNotFoundException(String.format("Usuário id= %s não encontrado!", id))
         );
     }
 
