4b9f92136089005d280ff50e1c4cd0eba4ef19bc
raulmaciel <raulmacielxd@gmail.com>
2025-06-03 10:34:01 -0300

    feat: documenta o recursos para alterar a senha e documenta o recurso listar todos os usuários

--- src/main/java/dev/raul/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/dev/raul/demo_park_api/web/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import dev.raul.demo_park_api.web.dto.UsuarioSenhaDto;
 import dev.raul.demo_park_api.web.dto.mapper.UsuarioMapper;
 import dev.raul.demo_park_api.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -48,18 +49,58 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
+
+    @Operation(summary = "Recuperar um usuário pelo id"
+            , description = "Recuperar um usuário pelo id"
+            , responses = {
+            @ApiResponse(responseCode = "200"
+                    , description = "Recurso recuperado com sucesso"
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = UsuarioResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado"
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = ErrorMessage.class)))
+    }
+
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id){
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
+    @Operation(summary = "Atualizar senhha"
+            , description = "Atualizar senhha"
+            , responses = {
+            @ApiResponse(responseCode = "204"
+                    , description = "Senha atualizada com sucesso"
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = Void.class))),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado"
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "400", description = "Senha não confere"
+                    , content = @Content(mediaType = "application/json"
+                    , schema = @Schema(implementation = ErrorMessage.class)))
+    }
+
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto){
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return ResponseEntity.noContent().build();
     }
 
+
+    @Operation(summary = "Listar todos os usuários"
+            , description = "Listar todos os usuários"
+            , responses = {
+            @ApiResponse(responseCode = "200"
+                    , description = "Recurso recuperado com sucesso"
+                    , content = @Content(mediaType = "application/json"
+                    , array = @ArraySchema(schema = @Schema(implementation = UsuarioResponseDto.class))))
+        }
+    )
     @GetMapping
     public ResponseEntity<List<UsuarioResponseDto>> getAll(){
         List<Usuario> usuarios = usuarioService.buscarTodos();
