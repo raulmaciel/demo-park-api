9f851ff644d7a3a758b8851fc82f607db0db1008
raulmaciel <raulmacielxd@gmail.com>
2025-06-04 09:59:49 -0300

    config: configura o ambiente de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'maria@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'jubileia@email.com', '123456', 'ROLE_CLIENTE');
