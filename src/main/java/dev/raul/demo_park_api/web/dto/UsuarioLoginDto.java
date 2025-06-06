a08a8652be47c3ae917799e53cf0c2234b32778f
raulmaciel <raulmacielxd@gmail.com>
2025-06-06 14:14:06 -0300

    feat: cria o controller de Autenticação

---
+++ src/main/java/dev/raul/demo_park_api/web/dto/UsuarioLoginDto.java
@@ -0,0 +1,22 @@
+package dev.raul.demo_park_api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioLoginDto {
+    @NotBlank
+    @Email(message = "Formato do e-mail inválido!", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
