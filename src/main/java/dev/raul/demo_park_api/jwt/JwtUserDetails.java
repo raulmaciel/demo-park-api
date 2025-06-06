46069d2215c8e3e3f5d5033b274f24f6b935ac7d
raulmaciel <raulmacielxd@gmail.com>
2025-06-06 10:56:33 -0300

    feat: cria a classe JwtUserDetails

---
+++ src/main/java/dev/raul/demo_park_api/jwt/JwtUserDetails.java
@@ -0,0 +1,28 @@
+package dev.raul.demo_park_api.jwt;
+
+import dev.raul.demo_park_api.entity.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class JwtUserDetails extends User {
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername()
+                , usuario.getPassword()
+                , AuthorityUtils.createAuthorityList(usuario.getRole().name())
+        );
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
+}
