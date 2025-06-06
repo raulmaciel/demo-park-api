0c2ec70ab242e05b675e565c0c4b5d9d01120a5e
raulmaciel <raulmacielxd@gmail.com>
2025-06-06 11:33:37 -0300

    feat: cria a classe JwtUserDetailsService e os metodos buscarRolePorUsername e buscarPorUsername

--- src/main/java/dev/raul/demo_park_api/repository/UsuarioRepository.java
+++ src/main/java/dev/raul/demo_park_api/repository/UsuarioRepository.java
@@ -2,7 +2,15 @@ package dev.raul.demo_park_api.repository;
 
 import dev.raul.demo_park_api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByUsername(String username);
+
+
+    @Query("select u.role from Usuario u where u.username like :username")
+    Usuario.Role findRoleByUsername(String username);
 }
--- src/main/java/dev/raul/demo_park_api/service/UsuarioService.java
+++ src/main/java/dev/raul/demo_park_api/service/UsuarioService.java
@@ -53,4 +53,17 @@ public class UsuarioService {
     public List<Usuario> buscarTodos() {
         return usuarioRepository.findAll();
     }
+
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorUsername(String username) {
+        return usuarioRepository.findByUsername(username).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuário '%s' não encontrado", username))
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario.Role buscarRolePorUsername(String username) {
+        return usuarioRepository.findRoleByUsername(username);
+    }
 }
